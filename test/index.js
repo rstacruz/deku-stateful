03a4283f1846ec5daed0aa55af5bb423e856a21b
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-08-15 09:48:58 +0800

    Refactor tests with tape-plus

--- test/index.js
+++ test/index.js
@@ -1,99 +1,94 @@
-var test = require('tape')
+require('jsdom-global')()
+
 var deku = require('deku')
 var stateful = require('../index')
 var h = deku.element
+var describe = require('tape-plus').describe
 
-test('invoking jsdom', require('jsdom-global/tape')())
+describe('deku-stateful', function (it) {
+  var el
 
-test('stateful', function (t) {
-  var el = document.createElement('div')
-  var render = deku.dom.createRenderer(el, dispatch)
-  function dispatch () { render(h(component)) }
-
-  var component = stateful({
-    render: function (model) {
-      return h('div', {}, 'hello world')
-    }
+  it.beforeEach(function (t) {
+    el = document.createElement('div')
   })
 
-  dispatch()
-
-  t.equal(el.innerHTML, '<div>hello world</div>', 'first render')
-  t.end()
-})
-
-test('dumb components', function (t) {
-  var el = document.createElement('div')
-  var render = deku.dom.createRenderer(el, dispatch)
-  function dispatch () { render(h(component)) }
-
-  var component = stateful(function (model) {
-    return h('div', {}, 'hello world')
-  })
-
-  dispatch()
-
-  t.equal(el.innerHTML, '<div>hello world</div>', 'first render')
-  t.end()
-})
-
-test('initialState', function (t) {
-  var el = document.createElement('div')
-  var render = deku.dom.createRenderer(el, dispatch)
-  function dispatch () { render(h(component)) }
-
-  t.plan(2)
-
-  var component = stateful({
-    initialState: function (model) {
-      t.pass('initialstate called')
-      return { name: 'jake' }
-    },
-
-    render: function (model) {
-      return h('div', {}, 'hello ', model.state.name)
-    }
-  })
-
-  dispatch()
-
-  t.equal(el.innerHTML, '<div>hello jake</div>', 'first render')
-  t.end()
-})
-
-test('set state', function (t) {
-  t.plan(4) // 2 dispatch, 2 renders
-  var el = document.createElement('div')
-  var render = deku.dom.createRenderer(el, dispatch)
-
-  function dispatch () {
-    t.pass('dispatch called')
-    render(h(component))
+  function render (component) {
+    var render = deku.dom.createRenderer(el, dispatch)
+    function dispatch () { render(h(component)) }
+    dispatch()
   }
 
-  var component = stateful({
-    initialState: function (model) {
-      return { name: 'jake' }
-    },
+  it('works', function (t) {
+    var component = stateful({
+      render: function (model) {
+        return h('div', {}, 'hello world')
+      }
+    })
 
-    render: function (model) {
-      setTimeout(function () {
-        if (model.state.name === 'jake') {
-          model.setState({ name: 'john' })
-        }
-      })
-      return h('div', {}, 'hello ', model.state.name)
-    }
+    render(component)
+    t.equal(el.innerHTML, '<div>hello world</div>', 'first render')
   })
 
-  dispatch()
+  it('works with pure components', function (t) {
+    function component (model) {
+      return h('div', {}, 'hello world')
+    }
 
-  t.equal(el.innerHTML, '<div>hello jake</div>', 'first render')
+    render(component)
+    t.equal(el.innerHTML, '<div>hello world</div>', 'first render')
+  })
 
-  setTimeout(function () {
-    t.equal(el.innerHTML, '<div>hello john</div>', 'next render')
-    t.end()
-  }, 100)
+  it('supports initialState', function (t) {
+    t.plan(2)
+
+    var component = stateful({
+      initialState: function (model) {
+        t.pass('initialstate called')
+        return { name: 'jake' }
+      },
+
+      render: function (model) {
+        return h('div', {}, 'hello ', model.state.name)
+      }
+    })
+
+    render(component)
+    t.equal(el.innerHTML, '<div>hello jake</div>', 'first render')
+  })
+
+  it('supports setState', function (t, end) {
+    t.plan(4) // 2 dispatch, 2 renders
+    var render = deku.dom.createRenderer(el, dispatch)
+
+    function dispatch () {
+      t.pass('dispatch called')
+      render(h(component))
+    }
+
+    var component = stateful({
+      initialState: function (model) {
+        return { name: 'jake' }
+      },
+
+      render: function (model) {
+        setTimeout(function () {
+          if (model.state.name === 'jake') {
+            model.setState({ name: 'john' })
+          }
+        })
+        return h('div', {}, 'hello ', model.state.name)
+      }
+    })
+
+    dispatch()
+
+    t.equal(el.innerHTML, '<div>hello jake</div>', 'first render')
+
+    setTimeout(function () {
+      t.equal(el.innerHTML, '<div>hello john</div>', 'next render')
+      end()
+    }, 100)
+  })
 })
 
-test('eslint', require('tape-eslint')())
+require('tape')('eslint', require('tape-eslint')())
